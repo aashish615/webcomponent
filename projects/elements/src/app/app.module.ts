95ffa3d93820cde9f549f118a3e982f2cc107405
Ashish Pillai <aashish615@gmail.com>
2022-10-29 01:57:23 +0530

    remove appcomponent from bootsrap array

--- projects/elements/src/app/app.module.ts
+++ projects/elements/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Injector, NgModule } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
+import { PopupComponent, PopupModule } from 'projects/popup/src/public-api';
 // import { PopupComponent } from 'projects/popup/src/lib/popup.component';
 // import { PopupModule } from 'projects/popup/src/lib/popup.module';
-import { PopupComponent, PopupModule } from 'popup';
+// import { PopupComponent, PopupModule } from 'popup';
 
 import { AppComponent } from './app.component';
 
@@ -16,7 +17,7 @@ import { AppComponent } from './app.component';
     PopupModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap: [AppComponent]
 })
 export class AppModule {
 
